71fbd46bcf5f921a4de0ad2707c36073ce13f8b7
Caimano46 <danielekaimano@gmail.com>
2025-07-15 03:00:58 +0200

    Added a first broken version of the db and a new window

---
+++ UninaBioGarden/src/main/java/uninabiogarden/ui/AttivitàController.java
@@ -0,0 +1,37 @@
+package uninabiogarden.ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AttivitàController extends Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void btnCreaAttivià(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCreaTipo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnIndietro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
+
