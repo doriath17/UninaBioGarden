3bd6eba2b78548cfe9b41ca7d34ec925fa19412e
doriath17 <kep171024@gmail.com>
2025-08-06 17:15:41 +0200

    UI: minor changes to login view style

--- UninaBioGarden/src/main/java/uninabiogarden/ui/ControllerManager.java
+++ UninaBioGarden/src/main/java/uninabiogarden/ui/ControllerManager.java
@@ -24,6 +24,7 @@ public class ControllerManager {
   public void show(Stage stage) {
     this.stage = stage;
     Scene scene = new Scene(mainController.getRoot());
+    scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
     stage.setScene(scene);
     stage.show();
   }
