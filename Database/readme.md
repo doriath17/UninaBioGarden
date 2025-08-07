c36e744d9cc6351cd573541571a259d53a0e6bdf
doriath17 <kep171024@gmail.com>
2025-08-07 05:44:49 +0200

    Database: improved script.py

---
+++ Database/readme.md
@@ -0,0 +1,14 @@
+### Presentation
+
+These python scripts will be useful to populate the database.
+
+### Setup
+
+1. Create a virtual environment with `python3 -m venv .venv`
+2. Activate the environment in the terminal with `source ./.venv/bin/activate`.
+   In VSCode this can also be achieved by opening the palette (`ctrl + shift + p`), searching for `python: select interpreter` and choosing the one corresponding to `.venv`.
+
+3. Installing the dependecies:
+
+- `pip install psycopg` library to connect to `postgresql`.
+- `pip install Faker` mock-data library.
---
+++ Database/script.py
@@ -0,0 +1,37 @@
+import csv 
+import psycopg
+
+def load_proprietari():
+  with open('proprietario.csv') as csv_file: 
+    reader = csv.DictReader(csv_file)
+    with psycopg.connect("dbname=uninabiogarden user=ubg_user") as conn:
+      with conn.cursor() as cur:
+        sql_values = '( VALUES\n'
+        for row in reader:
+          sql_values += (f"('{row['username']}', '{row['password']}', '{row['nome']}', '{row['cognome']}', '{row['bday']}', '{row['email']}', '{row['nazionalita']}', '{row['num_tel']}', '{row['residenza']}'),\n")
+
+        sql_values = sql_values[:-2]
+
+        sql = "INSERT INTO proprietario (username,password,nome,cognome,bday,email,nazionalita,num_tel,residenza)\n" + sql_values + ' )'
+
+        cur.execute(sql)
+
+def delete_proprietario(username):
+  with psycopg.connect("dbname=uninabiogarden user=ubg_user") as conn:
+    with conn.cursor() as cur:
+      cur.execute(f"DELETE FROM Proprietario WHERE username='{username}'")
+
+def load_coltivatori():
+  with open('coltivatore.csv') as csv_file: 
+    reader = csv.DictReader(csv_file)
+    with psycopg.connect("dbname=uninabiogarden user=ubg_user") as conn:
+      with conn.cursor() as cur:
+        sql_values = '( VALUES\n'
+        for row in reader:
+          sql_values += (f"('{row['username']}', '{row['password']}', '{row['nome']}', '{row['cognome']}', '{row['bday']}', '{row['email']}', '{row['nazionalita']}', '{row['num_tel']}', '{row['residenza']}'),\n")
+
+        sql_values = sql_values[:-2]
+
+        sql = "INSERT INTO coltivatore (username,password,nome,cognome,bday,email,nazionalita,num_tel,residenza)\n" + sql_values + ' )'
+
+        cur.execute(sql)
