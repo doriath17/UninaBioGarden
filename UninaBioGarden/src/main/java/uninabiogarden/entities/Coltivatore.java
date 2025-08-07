4840e9df8206a336f7be255e3f528952036245cf
doriath17 <kep171024@gmail.com>
2025-08-07 06:09:04 +0200

    minor changes to tabs

--- UninaBioGarden/src/main/java/uninabiogarden/entities/Coltivatore.java
+++ UninaBioGarden/src/main/java/uninabiogarden/entities/Coltivatore.java
@@ -3,7 +3,7 @@ package uninabiogarden.entities;
 import java.time.LocalDate;
 
 public class Coltivatore extends Utente {
-    public Coltivatore(String username, String password, String nome, String cognome, LocalDate bday, String nationality, String email, String numTel, String residenza) {
-        super(username, password, nome, cognome, bday, nationality, email, numTel, residenza);
-    }
+  public Coltivatore(String username, String password, String nome, String cognome, LocalDate bday, String nationality, String email, String numTel, String residenza) {
+    super(username, password, nome, cognome, bday, nationality, email, numTel, residenza);
+  }
 }
--- UninaBioGarden/src/main/java/uninabiogarden/entities/Utente.java
+++ UninaBioGarden/src/main/java/uninabiogarden/entities/Utente.java
@@ -4,138 +4,138 @@ import java.time.LocalDate;
 
 public abstract class Utente {
 
-    private String username;
-    private String password;
-    private String nome;
-    private String cognome;
-    private LocalDate bday;
-    private String nationality;
-    private String email;
-    private String numTel;
-    private String residenza;
+  private String username;
+  private String password;
+  private String nome;
+  private String cognome;
+  private LocalDate bday;
+  private String nationality;
+  private String email;
+  private String numTel;
+  private String residenza;
 
-    public static class Builder {
-        final String username;
-        final String password;
-        final String nome;
-        final String cognome;
-        final LocalDate bday;
-        final String nationality;
-        final String email;
-        String numTel;
-        String residenza;
+  public static class Builder {
+    final String username;
+    final String password;
+    final String nome;
+    final String cognome;
+    final LocalDate bday;
+    final String nationality;
+    final String email;
+    String numTel;
+    String residenza;
 
-        public Builder(String username, String password, String nome, String cognome, LocalDate bday, String nationality, String email) {
-            this.username = username;
-            this.password = password;
-            this.nome = nome;
-            this.cognome = cognome;
-            this.bday = bday;
-            this.nationality = nationality;
-            this.email = email;
-        }
-
-        public Builder numTel(String numTel) {
-            this.numTel = numTel;
-            return this;
-        }
-
-        public Builder residenza(String residenza) {
-            this.residenza = residenza;
-            return this;
-        }
-
-        public Proprietario buildProprietario() {
-            return new Proprietario(username, password, nome, cognome, bday, nationality, email, numTel, residenza);
-        }
-
-        public Coltivatore buildColtivatore() {
-            return new Coltivatore(username, password, nome, cognome, bday, nationality, email, numTel, residenza);
-        }
+    public Builder(String username, String password, String nome, String cognome, LocalDate bday, String nationality, String email) {
+      this.username = username;
+      this.password = password;
+      this.nome = nome;
+      this.cognome = cognome;
+      this.bday = bday;
+      this.nationality = nationality;
+      this.email = email;
     }
 
-    public Utente(String username, String password, String nome, String cognome, LocalDate bday, String nationality, String email, String numTel, String residenza) {
-        this.username = username;
-        this.password = password;
-        this.nome = nome;
-        this.cognome = cognome;
-        this.bday = bday;
-        this.nationality = nationality;
-        this.email = email;
-        this.numTel = numTel;
-        this.residenza = residenza;
+    public Builder numTel(String numTel) {
+      this.numTel = numTel;
+      return this;
     }
 
-    public String getUsername() {
-        return username;
+    public Builder residenza(String residenza) {
+      this.residenza = residenza;
+      return this;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public Proprietario buildProprietario() {
+      return new Proprietario(username, password, nome, cognome, bday, nationality, email, numTel, residenza);
     }
 
-    public String getPassword() {
-        return password;
+    public Coltivatore buildColtivatore() {
+      return new Coltivatore(username, password, nome, cognome, bday, nationality, email, numTel, residenza);
     }
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public Utente(String username, String password, String nome, String cognome, LocalDate bday, String nationality, String email, String numTel, String residenza) {
+    this.username = username;
+    this.password = password;
+    this.nome = nome;
+    this.cognome = cognome;
+    this.bday = bday;
+    this.nationality = nationality;
+    this.email = email;
+    this.numTel = numTel;
+    this.residenza = residenza;
+  }
 
-    public String getNome() {
-        return nome;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-    public String getCognome() {
-        return cognome;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-    public LocalDate getBday() {
-        return bday;
-    }
+  public String getNome() {
+    return nome;
+  }
 
-    public void setBday(LocalDate bday) {
-        this.bday = bday;
-    }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
-    public String getNationality() {
-        return nationality;
-    }
+  public String getCognome() {
+    return cognome;
+  }
 
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
+  public void setCognome(String cognome) {
+    this.cognome = cognome;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public LocalDate getBday() {
+    return bday;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setBday(LocalDate bday) {
+    this.bday = bday;
+  }
 
-    public String getNumTel() {
-        return numTel;
-    }
+  public String getNationality() {
+    return nationality;
+  }
 
-    public void setNumTel(String numTel) {
-        this.numTel = numTel;
-    }
+  public void setNationality(String nationality) {
+    this.nationality = nationality;
+  }
 
-    public String getResidenza() {
-        return residenza;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public void setResidenza(String residenza) {
-        this.residenza = residenza;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getNumTel() {
+    return numTel;
+  }
+
+  public void setNumTel(String numTel) {
+    this.numTel = numTel;
+  }
+
+  public String getResidenza() {
+    return residenza;
+  }
+
+  public void setResidenza(String residenza) {
+    this.residenza = residenza;
+  }
 
 }
